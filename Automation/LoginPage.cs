e3fc82a4a699dbae45b6d3e98ca21f7ebf8ed78b
A-Machagiri <adunurimachagiri@gmail.com>
2024-11-16 13:15:23 +0530

    Create Automation/LoginPage.cs

---
+++ Automation/LoginPage.cs
@@ -0,0 +1,23 @@
+// LoginPage using Page Object Model
+
+using OpenQA.Selenium;
+
+public class LoginPage
+{
+    private IWebDriver driver;
+    private By usernameField = By.Id("user-name");
+    private By passwordField = By.Id("password");
+    private By loginButton = By.Id("login-button");
+
+    public LoginPage(IWebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    public void Login(string username, string password)
+    {
+        driver.FindElement(usernameField).SendKeys(username);
+        driver.FindElement(passwordField).SendKeys(password);
+        driver.FindElement(loginButton).Click();
+    }
+}
