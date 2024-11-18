98d193adb7f2421d279170fc6a4bf192eda264d6
A-Machagiri <adunurimachagiri@gmail.com>
2024-11-18 10:37:05 +0530

    Update Test_Layer/LoginSteps.cs

--- Test_Layer/LoginSteps.cs
+++ Test_Layer/LoginSteps.cs
@@ -13,6 +13,36 @@ namespace Test_Layer
             _webUtilities = webUtilities;
         }
 
-        // Methods for login steps will be added here
+        using System;
+using OpenQA.Selenium;
+using PageObjects;
+using FluentAssertions;
+
+namespace Test_Layer
+{
+    public class LoginSteps
+    {
+        private WebUtilities _webUtilities;
+        private LoginPage _loginPage;
+
+        public LoginSteps(WebUtilities webUtilities)
+        {
+            _webUtilities = webUtilities;
+            _loginPage = new LoginPage(_webUtilities.GetDriver());
+        }
+
+        public void PerformLogin(string username, string password)
+        {
+            _loginPage.EnterCredentials(username, password);
+            _loginPage.ClickLoginButton();
+        }
+
+        public void VerifySuccessfulLogin()
+        {
+            var isLoggedIn = _loginPage.CheckIfLoggedIn();
+            isLoggedIn.Should().BeTrue("because the credentials are correct");
+        }
+    }
+}
     }
 }
