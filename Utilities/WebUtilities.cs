8d6b56196d43de98bfc2075d79c6fab6c21cef38
A-Machagiri <adunurimachagiri@gmail.com>
2024-11-18 10:36:48 +0530

    Update Utilities/WebUtilities.cs

--- Utilities/WebUtilities.cs
+++ Utilities/WebUtilities.cs
@@ -11,6 +11,61 @@ namespace Utilities
             _driver = driver;
         }
 
-        // Utility methods for WebDriver will be added here
+        using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
+
+namespace Utilities
+{
+    public class WebUtilities
+    {
+        private IWebDriver driver;
+
+        public IWebDriver InitializeDriver(string browserType)
+        {
+            switch (browserType.ToLower())
+            {
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "edge":
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    throw new ArgumentException("Browser not supported.");
+            }
+            return driver;
+        }
+
+        public void NavigateToUrl(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public IWebElement FindElement(By by)
+        {
+            return driver.FindElement(by);
+        }
+
+        public void Click(By by)
+        {
+            FindElement(by).Click();
+        }
+
+        public void SendKeys(By by, string text)
+        {
+            FindElement(by).SendKeys(text);
+        }
+
+        public void Quit()
+        {
+            driver.Quit();
+        }
+    }
+}
     }
 }
