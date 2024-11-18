604db203f5dd667afa5d43bdcfb0cdbb60d824c4
A-Machagiri <adunurimachagiri@gmail.com>
2024-11-18 10:35:37 +0530

    Create Test_Layer/LoginSteps.cs

---
+++ Test_Layer/LoginSteps.cs
@@ -0,0 +1,18 @@
+using System;
+using OpenQA.Selenium;
+using PageObjects;
+
+namespace Test_Layer
+{
+    public class LoginSteps
+    {
+        private WebUtilities _webUtilities;
+
+        public LoginSteps(WebUtilities webUtilities)
+        {
+            _webUtilities = webUtilities;
+        }
+
+        // Methods for login steps will be added here
+    }
+}
