e9f66dd4f4821af24618bd9164be4699657b48f2
A-Machagiri <adunurimachagiri@gmail.com>
2024-11-18 10:36:58 +0530

    Update PageObjects/LoginPage.cs

--- PageObjects/LoginPage.cs
+++ PageObjects/LoginPage.cs
@@ -13,6 +13,37 @@ namespace PageObjects
             _webUtilities = webUtilities;
         }
 
-        // Properties for elements on the login page will be added here
+        using OpenQA.Selenium;
+
+namespace PageObjects
+{
+    public class LoginPage
+    {
+        private IWebDriver _driver;
+        private WebUtilities _webUtilities;
+
+        public LoginPage(IWebDriver driver, WebUtilities webUtilities)
+        {
+            _driver = driver;
+            _webUtilities = webUtilities;
+        }
+
+        public static By UsernameField = By.Id("user-name");
+        public static By PasswordField = By.Id("password");
+        public static By LoginButton = By.Id("login-button");
+        public static By ErrorMessage = By.CssSelector(".error-message-container");
+
+        public void EnterCredentials(string username, string password)
+        {
+            _webUtilities.SendKeys(UsernameField, username);
+            _webUtilities.SendKeys(PasswordField, password);
+        }
+
+        public void ClickLoginButton()
+        {
+            _webUtilities.Click(LoginButton);
+        }
+    }
+}
     }
 }
